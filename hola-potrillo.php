fa5fff8dc317e2f1ea843a03d961998e51aa2d93
Jose Melgares <roberto.melgares@ct.uneatlantico.es>
2026-02-23 14:05:34 +0100

    feat: adds phrase positioning

--- hola-potrillo.php
+++ hola-potrillo.php
@@ -13,12 +13,14 @@ if (!defined('ABSPATH')) {
 
 function get_phrase() {
     $phrases = array(
-        "Las mujeres ya no lloran, las mujeres facturan",
-        "Bebé, yo te quiero, pero es que yo me quiero más a mí",
-        "Creíste que me heriste y me volviste más dura" ,
-        "Una loba no ataca, se defiende" ,
-        "La gente de dos caras no la soporto",
-        "Verte con la nueva me dolió, pero ya estoy puesta pa' lo mío"
+        "Me dediqué a perderte, y me ausenté en momentos que se han ido para siempre... me dediqué a no verte",
+        "Como quien pierde una estrella, que se va y no volverá, así se fue mi esperanza",
+        "Mátalas con una sobredosis de ternura... mátalas con flores, con canciones",
+        "Y si un día glorioso en tus brazos acabo, qué felicidad... eso y más",
+        "Si tú supieras, que tu recuerdo me acaricia como el viento...",
+        "¡Ay amor! No cabe duda que sigo siendo sentimental...",
+        "Que seas muy feliz, mucho muy feliz, pero muy feliz, Pa' que no regreses",
+        "Fuiste ave de paso, y no sé por qué razón me fui acostumbrando cada día más a ti"
     );
     // returns a random phrase
     return $phrases[random_int(0, count($phrases)-1)];
@@ -29,4 +31,19 @@ function sacar_potrillo() {
     echo '<p id="potrillo">'.$phrase.'</p>';
 }
 
+function potrillo_css() {
+    echo "
+	<style type='text/css'>
+	#potrillo {
+		float: left;
+		padding-left: 15px;
+		padding-top: 5px;
+		margin: 0;
+		font-size: 11px;
+	}
+	</style>
+	";
+}
+
 add_action( 'admin_notices', 'sacar_potrillo');
+add_action('admin_head','potrillo_css');
